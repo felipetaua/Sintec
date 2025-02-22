91097e9d961aee1fc9d6a74e69fc5b721129d0d1
felipetaua <taua.felipee@gmail.com>
2025-02-22 01:04:03 -0300

    carregamento

--- loading.js
+++ loading.js
@@ -1,1 +1,13 @@
-
+// Função para exibir a tela de carregamento
+function loading() {
+    const loadingElement = document.querySelector(".loading"); // Seleciona o elemento de carregamento
+  
+    // Exibe a tela de carregamento enquanto a página carrega
+    loadingElement.style.display = 'block';
+  
+    // Quando toda a página estiver carregada, a tela de carregamento será removida
+    window.onload = () => {
+      loadingElement.style.display = 'none'; // Esconde a tela de carregamento
+    };
+  }
+  
